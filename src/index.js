765ec16e1de79a454ae295d69702b28304d2fecc
Stepan <103524021+Stepan-Melnyk@users.noreply.github.com>
2022-06-27 23:35:20 +0300

    script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import modalOrder from './js/modal_buy-now';
 import modalLocation from './js/modal-locations';
 import reviewsSlider from './js/reviews-slider';
+import burgerMenu from './js/mobile-menu';
