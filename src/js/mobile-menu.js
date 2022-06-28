1cdd09e3304216bf52a8ecc6aa8e829fe1a6f9a2
Stepan <103524021+Stepan-Melnyk@users.noreply.github.com>
2022-06-28 14:59:10 +0300

    js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,12 +2,14 @@
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
-  
+    const closeMenuModal = document.querySelector('.js-close'); 
+
     const toggleMenu = () => {
       const isMenuOpen =
         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
+      mobileMenu.classList.toggle('is-close');
   
       const scrollLockMethod = !isMenuOpen
         ? 'disableBodyScroll'
@@ -17,7 +19,8 @@
   
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
-  
+    closeMenuModal.addEventListener('click', toggleMenu);
+
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
